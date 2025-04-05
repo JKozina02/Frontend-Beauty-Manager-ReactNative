480220e4b6a155a45f1034bf2f2a6ec6656f14e1
bogdanovich231 <kulinkovich56@gmail.com>
2025-04-05 16:58:55 +0200

    refactor: add key word for commits

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -2,6 +2,6 @@ module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "type-empty": [2, "never"],
-    "type-enum": [2, "always", ["ci", "feat", "fix", "refactor", "revert", "style"]],
+    "type-enum": [2, "always", ["ci", "feat", "init", "docs", "fix", "test", "refactor", "revert", "style"]],
   },
 };
