a8900ee9277acf84a9ec2ece1545459d850f3148
AlGoldyn <al_goldyn@wp.pl>
2025-04-22 11:07:12 +0200

    feat: add menuComponent and mainScreen ph and nav

---
+++ src/screens/MainScreen.js
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const MainScreen = () => {
+  return (
+    <View>
+      <Text> MainPlaceHolder</Text>
+    </View>
+  );
+};
--- src/screens/SignInScreen.js
+++ src/screens/SignInScreen.js
@@ -1,5 +1,26 @@
-import { Text } from "react-native";
+import { CustomButton } from "../components/buttons/CustomButton";
+import { useNavigation } from "@react-navigation/native";
+import { StyleSheet, View } from "react-native";
 
 export const SignInScreen = () => {
-  return <Text>Sign in Screen</Text>;
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.wrapperButtons}>
+      <CustomButton
+        onPress={() => navigation.navigate("MainScreen")}
+        title="MainScreen"
+        color={"#000000"}
+        backgroundColor={"#FFFAFC"}
+      />
+    </View>
+  );
 };
+const styles = StyleSheet.create({
+  wrapperButtons: {
+    flexDirection: "row",
+    justifyContent: "space-between",
+    gap: 15,
+    marginTop: 40,
+  },
+});
