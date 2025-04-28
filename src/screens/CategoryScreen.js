17c57b579e9a0f5ec8b90ee8e7cf708263259da8
nataliasuda <natalia.suda7@wp.pl>
2025-04-28 16:30:40 +0200

    feat: add CategoryScreen using the product component with sample data

---
+++ src/screens/CategoryScreen.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { View, StyleSheet, ScrollView } from "react-native";
+import ProductComponent from "../components/ui/ProductComponent";
+const CategoryScreen = () => {
+  const dummyData = {
+    id: "12345",
+    image:
+      "https://images.kikocosmetics.com/lf2wbbijeo86/1Vx6ers0mjdfH9ESN9PhPl/28098bd3f6d1b7b94a6f69d19a96e692/Info_BeautyServices_NEW-Landing-Header_MakeupAndGo.jpg?w=1920&fm=webp", // przykładowy obrazek
+    name: "Salon Artist",
+    address: "Tęczowa, Wrocław",
+    rating: 4.5,
+  };
+
+  return (
+    <ScrollView contentContainerStyle={styles.container}>
+      <ProductComponent id={dummyData.id} image={dummyData.image} name={dummyData.name} address={dummyData.address} />
+    </ScrollView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 90,
+  },
+});
+
+export default CategoryScreen;
