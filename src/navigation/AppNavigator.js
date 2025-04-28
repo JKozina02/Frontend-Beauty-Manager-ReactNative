13f470d63774358ee24de46407ba00882f48489f
nataliasuda <natalia.suda7@wp.pl>
2025-04-28 16:34:33 +0200

    feat: add navigation to ServiceDetailsScreen

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -2,6 +2,7 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { HomeScreen } from "../screens/HomeScreen";
 import { SignInScreen } from "../screens/SignInScreen";
 import { SignUpScreen } from "../screens/SignUpSreen";
+import { ServiceDetailsScreen } from "../screens/ServiceDetailsScreen";
 
 const Stack = createNativeStackNavigator();
 
@@ -11,6 +12,7 @@ export const AppNavigator = () => {
       <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       <Stack.Screen name="SignIn" component={SignInScreen} options={{ headerShown: false }} />
       <Stack.Screen name="SignUp" component={SignUpScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="ServiceDetailsScreen" component={ServiceDetailsScreen} options={{ headerShown: false }} />
     </Stack.Navigator>
   );
 };
