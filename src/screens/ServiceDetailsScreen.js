d354b8d605d228b767601044595075a80c5669b3
nataliasuda <natalia.suda7@wp.pl>
2025-04-28 16:24:27 +0200

    feat: add ServiceDetailsScreen

---
+++ src/screens/ServiceDetailsScreen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+export const ServiceDetailsScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.text}>Service Details Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+ 
+  
+});
