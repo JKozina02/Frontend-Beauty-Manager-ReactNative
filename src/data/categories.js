8f98a54c0504b8c6f536b491f0ce4ca911502137
nataliasuda <natalia.suda7@wp.pl>
2025-05-09 00:26:37 +0200

    feat: add file with category data

---
+++ src/data/categories.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    id_category: "1",
+    name: "Eyelashes",
+    image: require("../../assets/category/eyelashes.png"),
+  },
+  {
+    id_category: "2",
+    name: "Nails",
+    image: require("../../assets/category/nails.png"),
+  },
+  {
+    id_category: "3",
+    name: "Eyebrows",
+    image: require("../../assets/category/eyebrows.png"),
+  },
+  {
+    id_category: "4",
+    name: "Makeup",
+    image: require("../../assets/category/makeup.png"),
+  },
+];
