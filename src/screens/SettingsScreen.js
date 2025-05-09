b371d32c783b385abd120486b9a291a8cedabc68
nataliasuda <natalia.suda7@wp.pl>
2025-05-09 02:10:17 +0200

    feat: add city and language change fields

---
+++ src/screens/SettingsScreen.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { View, Text, Pressable, StyleSheet } from "react-native";
+
+export const SettingsScreen = ({ navigation }) => {
+  return (
+    <View style={styles.container}>
+      <Pressable style={styles.selectionBox}>
+        <Text style={styles.selectionText}>Wroclaw</Text>
+      </Pressable>
+
+      <Pressable style={styles.selectionBox}>
+        <Text style={styles.selectionText}>English</Text>
+      </Pressable>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFFFFF",
+    paddingHorizontal: 20,
+    paddingTop: 40,
+  },
+  selectionBox: {
+    backgroundColor: "#F5F5F5",
+    padding: 16,
+    borderRadius: 15,
+    marginBottom: 26,
+  },
+  selectionText: {
+    fontSize: 18,
+  },
+});
