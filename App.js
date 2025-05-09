ab66910682e83417af1affebdeababcc3ace6c89
nataliasuda <natalia.suda7@wp.pl>
2025-05-09 14:20:23 +0200

    feat: add i18next config and enable automatic language detection

--- App.js
+++ App.js
@@ -1,9 +1,27 @@
-import { Provider, useSelector } from "react-redux";
+import { Provider } from "react-redux";
 import { store } from "./src/store/store";
 import { AppNavigator } from "./src/navigation/AppNavigator";
 import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import { useEffect, useState } from "react";
 import { loadFonts } from "./src/utils/loadFonts/loadFonts";
+import i18n from "i18next";
+import { I18nextProvider } from "react-i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+i18n
+  .use(initReactI18next)
+  .use(LanguageDetector)
+  .init({
+    fallbackLng: "en",
+    detection: {
+      order: ["navigator"],
+      caches: ["localStorage"],
+    },
+    interpolation: {
+      escapeValue: false,
+    },
+  });
 
 export default function App() {
   const [fontsLoaded, setFontsLoaded] = useState(false);
@@ -17,10 +35,12 @@ export default function App() {
   }
 
   return (
-    <Provider store={store}>
-      <NavigationContainer theme={DefaultTheme}>
-        <AppNavigator />
-      </NavigationContainer>
-    </Provider>
+    <I18nextProvider i18n={i18n}>
+      <Provider store={store}>
+        <NavigationContainer theme={DefaultTheme}>
+          <AppNavigator />
+        </NavigationContainer>
+      </Provider>
+    </I18nextProvider>
   );
 }
